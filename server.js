a400fb69614712dda5a7e4c0302b3253d1c90140
Junior Ngangeli <juniorngangeli3@gmail.com>
2022-03-30 13:34:45 +0100

    feat: update swagger servers list

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const multer = require('multer');
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config/config')[env];
+require("dotenv").config();
+const multer = require("multer");
+const env = process.env.NODE_ENV || "development";
+const config = require("./config/config")[env];
 
-const { exec } = require('child_process');
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const { exec } = require("child_process");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -18,20 +18,24 @@ app.use(cookieParser());
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: 'Golegal API',
-      description: 'Golegal API description',
+      title: "Golegal API",
+      description: "Golegal API description",
       contact: {
-        name: '',
+        name: "",
       },
-      servers: ['http://localhost:8000', 'https://golegalapi.herokuapp.com'],
+      servers: [
+        "https://golegal.tech",
+        "http://localhost:8000",
+        "https://golegalapi.herokuapp.com",
+      ],
     },
   },
   //['.routes/*.js']
-  apis: ['server.js'],
+  apis: ["server.js"],
 };
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use(
-  '/api/golegal/documentation',
+  "/api/golegal/documentation",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocs)
 );
@@ -106,14 +110,14 @@ app.use(
  *          500:
  *            description: Failure of response
  */
-app.use('/api/golegal', require('./routes/ordersRoutes'));
+app.use("/api/golegal", require("./routes/ordersRoutes"));
 
 // Upload file
 // SET STORAGE
-const regex = new RegExp('[^.]+$');
+const regex = new RegExp("[^.]+$");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads');
+    cb(null, "uploads");
   },
   filename: function (req, file, cb) {
     const extension = file.originalname.match(regex);
@@ -123,33 +127,33 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 app.post(
-  '/api/golegal/uploadfile',
-  upload.single('script'),
+  "/api/golegal/uploadfile",
+  upload.single("script"),
   async (req, res, next) => {
     try {
       const file = req.file;
       // const client = new Client();
-      if (!file) return res.status(500).json({ msg: 'Please upload a file' });
+      if (!file) return res.status(500).json({ msg: "Please upload a file" });
 
       const extension = file.originalname.match(regex);
-      if (extension != 'sql')
-        return res.status(400).json({ msg: 'Please upload a sql file' });
-      const path = __dirname + '/uploads/' + file.originalname;
+      if (extension != "sql")
+        return res.status(400).json({ msg: "Please upload a sql file" });
+      const path = __dirname + "/uploads/" + file.originalname;
 
       const cmd =
-        'set PGPASSWORD=' +
+        "set PGPASSWORD=" +
         config.password +
-        '&& psql -h beccqwphapuxxlayapg8-postgresql.services.clever-cloud.com -p 5432 -U ued4nrxegaud0kgua4cx -d beccqwphapuxxlayapg8 -f uploads/' +
+        "&& psql -h beccqwphapuxxlayapg8-postgresql.services.clever-cloud.com -p 5432 -U ued4nrxegaud0kgua4cx -d beccqwphapuxxlayapg8 -f uploads/" +
         file.originalname;
       exec(cmd, (err, stdout, stderr) => {
         if (err) {
           return res.status(500).json({ Err: err });
         }
-        res.status(301).sendFile(__dirname + '/index.html');
+        res.status(301).sendFile(__dirname + "/index.html");
       });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
